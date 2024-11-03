97f9f824a66962614874e5353d552bfebbc00e62
Bec28 <zeldaisgood007@gmail.com>
2024-11-02 22:47:21 -0300

    Change it back to the initial

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 function sayHello() {
-    alert('Hello, welcome to my website!');
+  alert('Hello, welcome to my website!');
 }
-
-function dontclick() {
-    alert('I told you la still click!');
-}
-
-window.addEventListener('scroll', () => {
-  let scrollRatio = window.scrollY / document.body.scrollHeight;
-  document.body.style.background = `linear-gradient(to bottom, #6eb5ff, ${scrollRatio > 0.5 ? '#4a83d3' : '#2a64b4'}, #093c81)`;
-});
