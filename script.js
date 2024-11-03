6f419fdce017b13baab75079b81172a70f7ccbb6
Bec28 <zeldaisgood007@gmail.com>
2024-11-03 01:18:39 -0400

    Make the ocean appear below, added author buttom

--- script.js
+++ script.js
@@ -6,7 +6,3 @@ function dontclick() {
   alert('I told you la still click!');
 }
 
-window.addEventListener('scroll', () => {
-let scrollRatio = window.scrollY / document.body.scrollHeight;
-document.body.style.background = `linear-gradient(to bottom, #6eb5ff, ${scrollRatio > 0.5 ? '#4a83d3' : '#2a64b4'}, #093c81)`;
-});
