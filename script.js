05972ce2f8b6873cd775a2cc3e0a16ff82380994
Bec28 <zeldaisgood007@gmail.com>
2024-11-02 22:34:04 -0300

    change the gradient background, add a author link and animation for it

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function sayHello() {
     alert('Hello, welcome to my website!');
 }
+
+function dontclick() {
+    alert('I told you la still click!');
+}
+
+window.addEventListener('scroll', () => {
+  let scrollRatio = window.scrollY / document.body.scrollHeight;
+  document.body.style.background = `linear-gradient(to bottom, #6eb5ff, ${scrollRatio > 0.5 ? '#4a83d3' : '#2a64b4'}, #093c81)`;
+});
