2f6565945f3511d80e3e76d7854ca493597fcb36
Bec28 <zeldaisgood007@gmail.com>
2024-11-03 11:15:51 -0400

    added info, changed the description

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function dontclick() {
   alert('I told you la still click!');
 }
 
+function author () {
+  alert("Becky Liu and Joshua Wu\nFrom Citadel High School")
+}
+
+function about () {
+  alert("2 days Hackathon project")
+}
