d98b461cf06dd97ab2ca87bc443d8ae40697a261
Bec28 <zeldaisgood007@gmail.com>
2024-11-03 14:59:42 -0400

    add animation to text and image

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function author () {
 function about () {
   alert("2 days Hackathon project")
 }
+
+document.addEventListener("scroll", function () {
+  const slidePopImage = document.querySelector(".slide-pop-image");
+  const rect = slidePopImage.getBoundingClientRect();
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+  if (rect.top <= windowHeight && rect.bottom >= 0) {
+      slidePopImage.classList.add("slide-pop-image"); // Trigger the animation
+  }
+});
